598198173aa91732c05da87e39492cee5363d7d9
toddcd <td3studios@gmail.com>
2021-02-02 17:56:09 +0000

    first commit for academy module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    
+    'summary' : """Academy application""",
+    
+    'description' : """ Academy Module to manage Training""",
+    
+    'author' : 'Todd',
+    
+    'website' : 'conversionlogix.com',
+    
+    'category' : 'Training',
+    
+    'version' : '0.1',
+    
+    'depends' : ['base'],
+    
+    'data' : [
+    
+    ],
+    
+    'demo' : [
+        
+    ]
+    
+}
